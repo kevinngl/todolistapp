0f0210f55c4d81cef69f4553e0f9ca2c24a4972c
kevinngl <if320021@students.del.ac.id>
2023-07-11 22:52:45 +0700

    //TODO 3
    Define room database class and prepopulate database using JSON

--- app/src/main/java/com/dicoding/todoapp/data/TaskDatabase.kt
+++ app/src/main/java/com/dicoding/todoapp/data/TaskDatabase.kt
@@ -1,6 +1,7 @@
 package com.dicoding.todoapp.data
 
 import android.content.Context
+import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 import com.dicoding.todoapp.R
@@ -12,6 +13,7 @@ import java.io.IOException
 import java.io.InputStreamReader
 
 //TODO 3 : Define room database class and prepopulate database using JSON
+@Database(entities = [Task::class], version = 1, exportSchema = false)
 abstract class TaskDatabase : RoomDatabase() {
 
     abstract fun taskDao(): TaskDao
